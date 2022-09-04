b921e7113c1c34211f9dc2edbed32a0287076206
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 05:23:15 +0900

    implement ets_semaphore

--- lib/ets_semaphore.ex
+++ lib/ets_semaphore.ex
@@ -2,4 +2,57 @@ defmodule EtsSemaphore do
   @moduledoc """
   Documentation for `EtsSemaphore`.
   """
+
+  alias :ets, as: Ets
+
+  @type t() :: atom()
+  @s :semaphore
+  @w :waiting_list
+
+  @spec new(t()) :: t()
+  def new(s) do
+    Ets.new(s, [:set, :public, :named_table])
+    s
+  end
+
+  def acquire(s) do
+    case Ets.update_counter(s, @s, [{2, 0}, {2, 1}], {@s, 0}) do
+      [0, _] ->
+        :ok
+
+      [_, _] ->
+        Ets.update_counter(s, @s, {2, -1})
+        w = Ets.update_counter(s, @w, {2, 1}, {@w, 0})
+        Ets.insert(s, {w, self()})
+
+        receive do
+          :signal ->
+            acquire(s)
+        end
+    end
+  end
+
+  def release(s) do
+    [_, _] = Ets.update_counter(s, @s, [{2, 0}, {2, -1, 0, 0}])
+    [w, _] = Ets.update_counter(s, @w, [{2, 0}, {2, -1, 0, 0}], {@w, 0})
+    release_sub(s, w)
+  end
+
+  defp release_sub(_s, 0), do: :ok
+
+  defp release_sub(s, w) do
+    case Ets.lookup(s, w) do
+      [{^w, pid}] ->
+        Ets.delete(s, w)
+        send(pid, :signal)
+
+      [] ->
+        :ok
+    end
+
+    release_sub(s, w - 1)
+  end
+
+  def delete(_s) do
+  end
 end
--- test/ets_semaphore_test.exs
+++ test/ets_semaphore_test.exs
@@ -14,4 +14,52 @@ defmodule EtsSemaphoreTest do
 
     assert_receive(:pong, 1_000)
   end
+
+  test "semaphore 1" do
+    s = :s
+
+    EtsSemaphore.new(s)
+    EtsSemaphore.acquire(s)
+    receiver = self()
+
+    _pid =
+      spawn(fn ->
+        EtsSemaphore.acquire(s)
+        EtsSemaphore.release(s)
+        send(receiver, :ping)
+      end)
+
+    refute_receive(:ping, 1_000)
+    EtsSemaphore.release(s)
+    assert_receive(:ping, 1_000)
+    EtsSemaphore.delete(s)
+  end
+
+  test "semaphore 2" do
+    s = :s
+
+    EtsSemaphore.new(s)
+    EtsSemaphore.acquire(s)
+    receiver = self()
+
+    _pid =
+      spawn(fn ->
+        EtsSemaphore.acquire(s)
+        EtsSemaphore.release(s)
+        send(receiver, :ping)
+      end)
+
+    _pid =
+      spawn(fn ->
+        EtsSemaphore.acquire(s)
+        EtsSemaphore.release(s)
+        send(receiver, :ping)
+      end)
+
+    refute_receive(:ping, 1_000)
+    EtsSemaphore.release(s)
+    assert_receive(:ping, 1_000)
+    assert_receive(:ping, 1_000)
+    EtsSemaphore.delete(s)
+  end
 end
