98464668483468040ca47ebf09e762c444fbb41d
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 03:49:14 +0900

    remove hello

--- lib/ets_semaphore.ex
+++ lib/ets_semaphore.ex
@@ -2,17 +2,4 @@ defmodule EtsSemaphore do
   @moduledoc """
   Documentation for `EtsSemaphore`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> EtsSemaphore.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/ets_semaphore_test.exs
+++ test/ets_semaphore_test.exs
@@ -1,8 +1,4 @@
 defmodule EtsSemaphoreTest do
   use ExUnit.Case
   doctest EtsSemaphore
-
-  test "greets the world" do
-    assert EtsSemaphore.hello() == :world
-  end
 end
