251db31f742e372ad1500dc3686ef8c6b8fb4148
Susumu Yamazaki <zacky1972@gmail.com>
2022-09-05 04:03:54 +0900

    add test "message passing"

--- test/ets_semaphore_test.exs
+++ test/ets_semaphore_test.exs
@@ -1,4 +1,17 @@
 defmodule EtsSemaphoreTest do
   use ExUnit.Case
   doctest EtsSemaphore
+
+  test "message passing" do
+    pid =
+      spawn(fn ->
+        receive do
+          {:ping, sender} -> send(sender, :pong)
+        end
+      end)
+
+    send(pid, {:ping, self()})
+
+    assert_receive(:pong, 1_000)
+  end
 end
